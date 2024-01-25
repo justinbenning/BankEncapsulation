af234d0894c79d915158ad32625335c7029c9b06
JustinBenning <justin.benning@gmail.com>
2024-01-25 18:58:19 -0500

    upload

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace BankEncapsulation
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount myBankAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            double depositAmount = double.Parse(Console.ReadLine());
+            myBankAccount.Deposit(depositAmount);
+            
+            Console.WriteLine($"Thank you your balance is now ${myBankAccount.GetBalance()}");
+        }
+    }
+    public class BankAccount
+    {
+        public BankAccount() { }
+        private double _balance = 0;
+       
+        //BankAccount MyBankAccount = new BankAccount();
+        
+        public void Deposit(double deposit)
+        {
+           _balance += deposit;
+         }
+      
+        public double GetBalance()
+        {
+            return _balance;
         }
     }
 }
