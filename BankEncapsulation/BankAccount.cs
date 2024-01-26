1034ed1df64dfc124445d4972145c11ee7d9a8ec
JustinBenning <justin.benning@gmail.com>
2024-01-26 12:04:42 -0500

    updated project

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount() { }
+        private double _balance = 0;
+
+        //BankAccount MyBankAccount = new BankAccount();
+
+        public void Deposit(double deposit)
+        {
+            _balance += deposit;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -13,22 +13,5 @@ namespace BankEncapsulation
             
             Console.WriteLine($"Thank you your balance is now ${myBankAccount.GetBalance()}");
         }
-    }
-    public class BankAccount
-    {
-        public BankAccount() { }
-        private double _balance = 0;
-       
-        //BankAccount MyBankAccount = new BankAccount();
-        
-        public void Deposit(double deposit)
-        {
-           _balance += deposit;
-         }
-      
-        public double GetBalance()
-        {
-            return _balance;
-        }
-    }
+    } 
 }
